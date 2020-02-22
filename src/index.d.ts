60f7ba2f956899b73146149be9beb7d021e97766
Arun <palerdot@gmail.com>
2020-02-22 21:06:48 +0530

    [typescript-support] use proper vue types for vue speedometer

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,45 @@
-../lib/d3-speedometer/src/index.d.ts
+import Vue, { VNode } from "vue"
+
+declare module "vue-speedometer" {
+  interface ComponentOptions<V extends Vue> {
+    value?: number
+
+    minValue?: number
+    maxValue?: number
+
+    segments?: number
+    maxSegmentLabels?: number
+
+    forceRender?: boolean
+
+    width?: number
+    height?: number
+
+    dimensionUnit?: string
+    fluidWidth?: boolean
+
+    needleColor?: string
+    startColor?: string
+    endColor?: string
+    segmentColors?: string[]
+
+    needleTransition?: string
+    needleTransitionDuration?: number
+    needleHeightRatio?: number
+
+    ringWidth?: number
+    textColor?: string
+
+    valueFormat?: string
+    currentValueText?: string
+    currentValuePlaceholderStyle?: string
+
+    customSegmentStops?: number[]
+
+    labelFontSize?: string
+    valueTextFontSize?: string
+
+    paddingHorizontal?: number
+    paddingVertical?: number
+  }
+}
