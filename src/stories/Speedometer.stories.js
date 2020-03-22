9ccc6eb4985e014db239b8d6bbdcffe898b2b326
Arun <palerdot@gmail.com>
2020-03-22 20:01:41 +0530

    [CORE][0.10.0]

--- src/stories/Speedometer.stories.js
+++ src/stories/Speedometer.stories.js
@@ -10,6 +10,82 @@ export const DefaultWithNoConfig = () =>
 export const ConfiguringValues = () =>
   `<vue-speedometer :value="333" textColor="${textColor}" />`
 
+export const CustomSegmentLabels = () => `
+  <div>
+    <vue-speedometer 
+      :width="500"
+      :needleHeightRatio="0.7"
+      :value="777"
+      :customSegmentStops='[0, 250, 750, 1000]'
+      :segmentColors='["#9399ff", "#14ffec", "#00bbf0"]'
+      currentValueText="Happiness Level"
+      :customSegmentLabels='[
+        {
+          text: "Very Bad",
+          position: "INSIDE",
+          color: "#555",
+        },
+        {
+          text: "Bad",
+          position: "INSIDE",
+          color: "#555",
+        },
+        {
+          text: "Ok",
+          position: "INSIDE",
+          color: "#555",
+          fontSize: "19px",
+        },
+        {
+          text: "Good",
+          position: "INSIDE",
+          color: "#555",
+        },
+        {
+          text: "Very Good",
+          position: "INSIDE",
+          color: "#555",
+        },
+      ]'
+      :ringWidth="47"
+      :needleTransitionDuration="3333"
+      needleTransition="easeElastic"
+      needleColor="#a7ff83"
+      textColor="#d8dee9"
+    />
+    <vue-speedometer 
+      :width="500"
+      :needleHeightRatio="0.7"
+      :value="777"
+      :customSegmentStops='[0, 250, 750, 1000]'
+      :segmentColors='["#9399ff", "#14ffec", "#00bbf0"]'
+      currentValueText="How are you?"
+      :customSegmentLabels='[
+        {
+          text: "Good",
+          position: "OUTSIDE",
+          color: "#d8dee9",
+        },
+        {
+          text: "Great",
+          position: "OUTSIDE",
+          color: "#d8dee9",
+        },
+        {
+          text: "Awesome!",
+          position: "OUTSIDE",
+          color: "#d8dee9",
+        },
+      ]'
+      :ringWidth="47"
+      :needleTransitionDuration="3333"
+      needleTransition="easeElastic"
+      needleColor="#a7ff83"
+      textColor="#d8dee9"
+    />
+  </div>
+`
+
 export const CustomSegmentColors = () => `
   <div>
     <vue-speedometer 
